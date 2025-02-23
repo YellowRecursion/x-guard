e460cf8d5773ee41e809e3b1e54d41939c0038d3
Vadim <zinnatullin.vadim2001@yandex.ru>
2025-02-23 22:12:53 +0500

    Builder added

---
+++ builder.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Configuration (можно менять между Release/Debug)
+set CONFIG=Release
+
+rem Пути
+set ROOT_DIR=%~dp0
+set BUILD_DIR=%ROOT_DIR%Build\
+
+rem Очистка и создание Build директории
+if exist "%BUILD_DIR%" (
+    rmdir /s /q "%BUILD_DIR%"
+)
+mkdir "%BUILD_DIR%"
+
+rem Список проектов для копирования (относительные пути)
+set PROJECTS[0]=XGuardKeepAlive\bin\%CONFIG%\net8.0
+set PROJECTS[1]=XGuardLauncher\bin\%CONFIG%\net8.0
+set PROJECTS[2]=XGuardLockScreen\bin\%CONFIG%\net8.0-windows
+set PROJECTS[3]=XGuardMain\bin\%CONFIG%\net8.0
+set PROJECTS[4]=XGuardScreenshoter\bin\%CONFIG%\net8.0-windows
+
+rem Копирование файлов из каждого проекта
+for /L %%i in (0,1,4) do (
+    set SRC_DIR=%ROOT_DIR%!PROJECTS[%%i]!
+    if exist "!SRC_DIR!\" (
+        xcopy "!SRC_DIR!\*" "%BUILD_DIR%" /E /I /Y /Q
+    ) else (
+        echo Directory not found: !SRC_DIR!
+    )
+)
+
+echo Все файлы скопированы в: %BUILD_DIR%
+endlocal
